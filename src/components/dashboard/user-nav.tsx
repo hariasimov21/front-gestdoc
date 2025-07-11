e1b4b032f5d8c67c0e0e381230092c0230406718
Juan Mansilla Asenjo <juanmansillaasenjo@gmail.com>
2025-07-11 16:31:50 +0000

    el icono de logueado que sea la primera letra

--- src/components/dashboard/user-nav.tsx
+++ src/components/dashboard/user-nav.tsx
@@ -25,10 +25,7 @@ interface UserNavProps {
 export function UserNav({ userName, userEmail }: UserNavProps) {
     const getInitials = (name: string) => {
         if (!name) return '';
-        const names = name.split(' ');
-        if (names.length === 0) return '';
-        if (names.length === 1) return names[0].substring(0, 2).toUpperCase();
-        return (names[0][0] + names[names.length - 1][0]).toUpperCase();
+        return name.charAt(0).toUpperCase();
     }
 
   return (

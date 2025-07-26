430c7ce72b7911ea8b53f58b98a955e5456c9fc4
Juan Mansilla Asenjo <juanmansillaasenjo@gmail.com>
2025-07-26 15:35:54 +0000

    y que se vea bien en movile

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import { LoginForm } from '@/components/auth/login-form';
 export default function LoginPage() {
   return (
     <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+      <div className="relative flex items-center justify-center h-56 lg:h-full flex-col bg-muted p-10 text-white dark:border-r">
         <div 
           className="absolute inset-0 bg-cover"
           style={{ backgroundImage: 'url(https://placehold.co/1080x1920.png)', backgroundPosition: 'center' }}
@@ -16,7 +16,7 @@ export default function LoginPage() {
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2 h-6 w-6"><path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"></path><polyline points="14 2 14 8 20 8"></polyline></svg>
           GestDoc
         </div>
-        <div className="relative z-20 mt-auto">
+        <div className="relative z-20 mt-auto hidden lg:block">
             <blockquote className="space-y-2">
                 <p className="text-lg">
                 “Esta herramienta ha transformado nuestra gestión de documentos. Es intuitiva, eficiente y totalmente indispensable para nuestras operaciones.”

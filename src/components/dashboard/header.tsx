7c2b2121234f754851545e32239d050fc2b0de43
Juan Mansilla Asenjo <juanmansillaasenjo@gmail.com>
2025-08-06 19:10:23 +0000

    que funcione como drawer el menu de la izuqierda en mobile con un boton

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -17,7 +17,9 @@ export function DashboardHeader({ userName, userEmail, sessionExp }: DashboardHe
   const isMobile = useIsMobile();
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background/95 px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 backdrop-blur-sm">
-      {isMobile && <SidebarTrigger />}
+      <div className="md:hidden">
+        <SidebarTrigger />
+      </div>
       
       <div className="ml-auto flex items-center space-x-2 sm:space-x-4">
         <UserNav userName={userName} userEmail={userEmail} sessionExp={sessionExp} />
